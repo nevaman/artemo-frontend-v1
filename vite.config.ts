7aebb2f8cf071a43d9df9f22b7e7e1e3384ba96a
nevaman <armonimsolomon@gmail.com>
2025-08-18 23:10:29 +0300

    Fix Development Server Setup

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  server: {
+    port: 5173,
+    host: true
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '.'),
