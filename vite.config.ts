4d5f5bb7cb343c486d1b4bcb2b72e9e7dc28452c
nevaman <armonimsolomon@gmail.com>
2025-08-18 23:11:56 +0300

    Fix Development Server Startup

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,15 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     port: 5173,
-    host: true
+    host: '0.0.0.0',
+    strictPort: true
   },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '.'),
     }
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom']
   }
 });
